3596a4817d326e177ce66e28743bdc540ef156f2
Marcos Martins <marcos2003mms@gmail.com>
2024-11-11 12:06:24 -0300

    Validate login and database config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB as FacadesDB;
 
 class AuthController extends Controller
 {
@@ -30,7 +33,38 @@ class AuthController extends Controller
         $usename = $request -> input('text_username');
         $password =  $request -> input('text_password');
 
-        echo 'ok';
+        $user = User::where("username", $usename)
+                    ->first();
+        
+        if(!$user){
+            return redirect()
+            ->back()
+            ->withInput()
+            ->with('loginError', 'username ou password incorreto');
+        }
+
+        if(!password_verify($password, $user->password)){
+            return redirect()
+            ->back()
+            ->withInput()
+            ->with('loginError', 'username ou password incorreto');
+        }
+
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+
+        session([
+            'user' => [
+                'id' => $user->id,
+             'username' => $user->username
+            ]
+            ]);
+
+            echo 'Login efetuado com sucesso';
+ 
+
+
     }
 
     public function logout(){
