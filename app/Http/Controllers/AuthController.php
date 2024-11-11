c4b4ad32c458dcb950bec2f056b04c369747952e
Marcos Martins <marcos2003mms@gmail.com>
2024-11-11 10:08:05 -0300

    add validation in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,27 @@ class AuthController extends Controller
         return view('login');
     }
     public function loginSubmit(Request $request){
-        echo 'Login Submit';
+
+
+        $request -> validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.requiered' => 'O username é obrigatorio',
+                'text_username.email' => 'E-mail invalido',
+                'text_password.required' => 'A senha é obrigatoria',
+                'text_password.min' => 'A senha deve ter :min ou mais caracteres',
+                'text_password.max' => 'A senha deve ter no maximo de :max caracteres'
+                
+            ]
+        );
+
+        $usename = $request -> input('text_username');
+        $password =  $request -> input('text_password');
+
+        echo 'ok';
     }
 
     public function logout(){
